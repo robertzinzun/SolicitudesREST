e2fb6f04e03cbdd385daf072ef37a70ada1cacae
Roberto <rsuarez@accitesz.com>
2023-05-03 10:29:47 -0500

    Version 2 de solicitudes

--- APISolicitudes/app.py
+++ APISolicitudes/app.py
@@ -1,8 +1,11 @@
 from flask import Flask,jsonify,request
 from V1.model import Opcion,db,Solicitud
+from V1.SolicitudesBPV1 import solicitudBP
 
 
 app=Flask(__name__)
+app.register_blueprint(solicitudBP)
+
 app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://titulatec_soa:Hola.123@localhost/TitulaTEC_SOA'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 
@@ -10,37 +13,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 def init():
     return {"mensaje":"Escuchando el Servicio REST de Solicitudes"}
 
-#Rutta para el listado general de solicitudes
-@app.route('/Solicitudes/v1',methods=['GET'])
-def listadoSolicitudes():
-    solicitud=Solicitud()
-    return solicitud.consultaGeneral()
-
-#Ruta para el listado indvidual de solicitudes en base al id de la solicitud
-@app.route('/Solicitudes/v1/<int:id>',methods=['GET'])
-def listarSolicitud(id):
-    solicitud=Solicitud()
-    return solicitud.consultaIndividual(id)
-
-#Ruta para agregar una solicitud
-@app.route('/Solicitudes/v1',methods=['POST'])
-def agregarSolicitud():
-    solicitud = Solicitud()
-    data=request.get_json()
-    return solicitud.agregar(data)
-
-#Ruta para editar los datos de una solicitud
-@app.route('/Solicitudes/v1',methods=['PUT'])
-def editarSolicitud():
-    solicitud=Solicitud()
-    data=request.get_json()
-    return solicitud.editar(data)
-
-#Ruta para eliminar una solicitud
-@app.route('/Solicitudes/v1/<int:id>',methods=['DELETE'])
-def eliminarSolicitud(id):
-    solicitud=Solicitud()
-    return solicitud.eliminar(id)
 
 #Ruta para el listado de las opciones disponibles para titulación
 @app.route('/opciones',methods=['GET'])
